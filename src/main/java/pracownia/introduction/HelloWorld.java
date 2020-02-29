ea3fd95829c22cb6e01c696eb83eeec24b4447d4
krystianrzepa-uam <kryrze@st.amu.edu.pl>
2020-02-29 10:31:03 +0100

    update

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,10 @@
 package pracownia.introduction;
-
+import org.apache.log4j.Logger;
 public class HelloWorld {
 
+    final static Logger logger = Logger.getLogger("name");
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        logger.info("message");
     }
 }
